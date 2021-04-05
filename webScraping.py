4b14f2b0d3b856197e029b35b9b96ae9e3c68dd6
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-05 17:37:39 -0500

    just print the element that I get in result

--- webScraping.py
+++ webScraping.py
@@ -6,3 +6,5 @@ import webbrowser
 url = requests.get("https://www.mercadolibre.com.mx/sony-playstation-5-825gb-standard-color-blanco-y-negro/p/MLM16171888?pdp_filters=category:MLM167860#searchVariation=MLM16171888&position=1&type=product&tracking_id=14123963-f2bb-450f-969e-96b2756268ce")
 soup = BeautifulSoup(url.content, "html.parser")
 result = soup.find('span', {'class':'price-tag-fraction'})
+
+print(result)
