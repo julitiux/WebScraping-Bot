e869ef1d9d2f68cbfa340511a7fa16c206ea3a49
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-05 17:34:13 -0500

    add a requests.get, just for get all the content of the site, in this case mercadolibre. With amazon dont work.

--- webScraping.py
+++ webScraping.py
@@ -2,3 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import webbrowser
+
+url = requests.get("https://www.mercadolibre.com.mx/sony-playstation-5-825gb-standard-color-blanco-y-negro/p/MLM16171888?pdp_filters=category:MLM167860#searchVariation=MLM16171888&position=1&type=product&tracking_id=14123963-f2bb-450f-969e-96b2756268ce")
