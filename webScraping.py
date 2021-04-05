bb20d83f4536f9b633cee052d0dd4f4ec855ae39
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-05 17:35:01 -0500

    parser all the content site with BeautifulSoup

--- webScraping.py
+++ webScraping.py
@@ -4,3 +4,4 @@ import time
 import webbrowser
 
 url = requests.get("https://www.mercadolibre.com.mx/sony-playstation-5-825gb-standard-color-blanco-y-negro/p/MLM16171888?pdp_filters=category:MLM167860#searchVariation=MLM16171888&position=1&type=product&tracking_id=14123963-f2bb-450f-969e-96b2756268ce")
+soup = BeautifulSoup(url.content, "html.parser")
